ce3b3b8d66f5eb43bd83e9551c67c8bb34cb8e6b
Juan Pablo Avendaño Duque <juanpisave27@gmail.com>
2024-03-08 18:44:58 -0500

    Mi primer commit

---
+++ src/com/juan/persona/app/Main.java
@@ -0,0 +1,9 @@
+package com.juan.persona.app;
+import com.juan.persona.dominio.Profesor;
+public class Main {
+    public static void main(String[] args) {
+        Profesor Alejandro = new Profesor("Alejandro Matrín");
+        System.out.println("Jueves de"+Alejandro.nombre);
+
+    }
+}
---
+++ src/com/juan/persona/dominio/Profesor.java
@@ -0,0 +1,7 @@
+package com.juan.persona.dominio;
+
+public class Profesor {
+    public String nombre;
+    public String cedula;
+    public Profesor(String nombre)
+}
