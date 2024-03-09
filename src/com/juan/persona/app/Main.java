1e54d0c507f06c10ec607aab01e10ffe9e779375
Juan Pablo Avendaño Duque <juanpisave27@gmail.com>
2024-03-08 22:08:15 -0500

    Ligeros cambios

--- src/com/juan/persona/app/Main.java
+++ src/com/juan/persona/app/Main.java
@@ -2,8 +2,14 @@ package com.juan.persona.app;
 import com.juan.persona.dominio.Profesor;
 public class Main {
     public static void main(String[] args) {
-        Profesor Alejandro = new Profesor("Alejandro Matrín");
-        System.out.println("Jueves de"+Alejandro.nombre);
+        Profesor alejandro = new Profesor("Alejandro Matrín", "12031",36);
+        Profesor saul = null;
+        saul = alejandro;
+        System.out.println("Jueves de "+alejandro.nombre+alejandro.cedula+alejandro.edad);
+        System.out.println("Jueves de "+saul.nombre+saul.cedula+saul.edad);
+        saul = new Profesor("Saul","123",31);
+        System.out.println("Jueves de "+saul.nombre+saul.cedula+saul.edad);
+
 
     }
 }
--- src/com/juan/persona/dominio/Profesor.java
+++ src/com/juan/persona/dominio/Profesor.java
@@ -2,5 +2,11 @@ package com.juan.persona.dominio;
 
 public class Profesor {
     public String nombre;
-    public String cdula;
+    public String cedula;
+    public int edad;
+    public Profesor(String nombre,String cedula, int edad) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+    }
 }
